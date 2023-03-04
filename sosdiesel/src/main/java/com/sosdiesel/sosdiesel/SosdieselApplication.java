89e300b78b74e5c0031e2db6807095dc3447a8d4
Luis Fernando <flimon04567@gmail.com>
2023-03-04 00:17:47 -0600

    front and backend full

---
+++ sosdiesel/src/main/java/com/sosdiesel/sosdiesel/SosdieselApplication.java
@@ -0,0 +1,13 @@
+package com.sosdiesel.sosdiesel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosdieselApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosdieselApplication.class, args);
+	}
+
+}
---
+++ sosdiesel/src/test/java/com/sosdiesel/sosdiesel/SosdieselApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosdiesel.sosdiesel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosdieselApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
